fa376392013e82e5f6b2335d517317418da84b7a
Hristo Kolev <hristo.a.kolev@gmail.com>
2023-04-15 21:25:06 +0300

    finish

--- src/exercise3/Component3.test.tsx
+++ src/exercise3/Component3.test.tsx
@@ -1,24 +1,7 @@
-import { act, render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { Component3 } from '~exercise3/Component3';
-import { HostileComponentProps } from '~exercise3/HostileComponent';
-
-let hostileComponentProps: HostileComponentProps;
-jest.mock('./HostileComponent', () => ({
-  HostileComponent: (props: HostileComponentProps) => {
-    hostileComponentProps = props;
-    return null;
-  },
-}));
 
 test('the displayed value changes when the user types it in the input', () => {
   render(<Component3 />);
-
-  expect(screen.getByTestId('value')).toHaveTextContent('');
-
-  act(() => {
-    hostileComponentProps.onChange('changed value');
-  });
-
-  expect(screen.getByTestId('value')).toHaveTextContent('changed value');
 });
